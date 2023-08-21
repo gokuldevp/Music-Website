6f09a1b55fb3467c5d91b77d6fe0b09f5c536291
Gokul Dev P <gokuldev.p123@gmail.com>
2023-08-21 18:00:49 +0530

    the toggle bar

--- js/script.js
+++ js/script.js
@@ -1,4 +1,14 @@
 let toggleNav = () => {
     const navbarItems = document.getElementById('navbarItems');
+    const navIcon = document.getElementById('navicon');
+    const forwardX = document.getElementById('forwardx');
+    const backwardX = document.getElementById('backwardx');
+
+    navIcon.classList.toggle('x-container');
+    backwardX.classList.toggle('x-backward');
+    backwardX.classList.toggle('bar');
+    forwardX.classList.toggle('x-forward');
+    forwardX.classList.toggle('bar');
+
     navbarItems.classList.toggle('show');
 }
